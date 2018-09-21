6bcf81b632c21eb326f8b44ef07f099bbd07ca3d
Dmitri Mendis <2dmendis@gmail.com>
2018-09-21 17:14:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 // Write your code in this file!
 const currentUser = 'Dmitri Mendis';
 const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
